7baac498322f7ff7f5c44f5fcf22b6818b724f9d
abutanda <abutanda@csub.edu>
2017-03-14 14:24:15 -0700

    add box for help

--- anaB.cpp
+++ anaB.cpp
@@ -5,9 +5,12 @@
 #include <iostream>
 #include <cstring>
 #include <stdlib.h>
+#include <GL/glx.h>
 extern"C"{
 #include "fonts.h"
 }
+extern int xres;
+extern int yres;
 using namespace std;
 void print_Ana()
 {
@@ -19,6 +22,15 @@ void print_Ana()
 void ana_show_help()
 {
     Rect r;
+    //float wid =120.f;
+    glColor3f(1.0,1.0,1.0);
+    
+    glBegin(GL_QUADS);
+    glVertex2i((xres/3),(yres/3));
+    glVertex2i((xres/3),(yres/3)*2);
+    glVertex2i((xres/3)*2,(yres/3)*2);
+    glVertex2i((xres/3)*2,(yres/3));	    
+    glEnd();
     r.bot = 400;
     r.left = 400;
     r.center = 0;
