3d0894a818862e80b3e62b99c4eed91a26ec4483
Tyrannosarous-Esteban <elopez60@csub.edu>
2017-04-18 13:32:53 -0700

    getting collision, math is wrong though

--- estebanL.cpp
+++ estebanL.cpp
@@ -58,26 +58,13 @@ void print_esteban()
 void render_floor()
 {
 	float w, h;
-	//try to make a brick size a factor of the pixel size
-	//32 is the size
 	float pixels = 64;
-	//eventually I want this to read from a text file
-	//this is just a test first
-	//glClear(GL_COLOR_BUFFER_BIT);
 	w=pixels;
 	h=pixels;
-	//probably should count the number of boxes as I fill the array
-	//for now just trying to get collision detections bw octopus
-	//and world floor
 	num_blocks_wide = xres/50;
-	//int arr[num_blocks_wide];
 	int arr[MAX_BLOCKS];
 	float pix_center = PIXEL_WIDTH/2;
 
-	//idea: use sea stories like moby dick and 10,000 leagues under the sea
-	//to create world blocks after doing a frequency analysis
-	//this if statement causes game to freeze somehow
-	//#define RTH //Random Tile Height
 #ifdef RTH
 	//trying to fill in array once
 	//this destroys the program, must learn to use a text file
@@ -91,46 +78,29 @@ void render_floor()
 	string filename = "level1.txt";
 	ifstream ifs;
 	int number;
-	//int total_boxes = 0;
-	//char c;
 	int i = 0;
+	if(!floor_set) {
 	ifs.open(filename.c_str());
-	//while (!ifs.eof()) {
 	while (ifs >> number && !ifs.eof()) {
-		//for (int i = 0; i < 25; i++) {
-		//ifs.get(c);
-		//arr[i] << c;
-		//c >> arr[i];
 		arr[i] = number;
 		total_boxes += number; 
 		i++;	
 	}
+	cout << "DEBUG: total_boxes = " << total_boxes << endl;
 	num_blocks_wide = i;
+	floor_set = 1;
 	ifs.close();
-	//for (int i = 0; i < 25; i++)
-	//cout << "INDEX " << i << " = " << arr[i] << endl;
+}
 
-	//read_by_char(filename);
 	int box_num=0;
 	for (int i = 0; i < num_blocks_wide; i++) {
 		platform_boxes[box_num].x = pix_center + (i*PIXEL_WIDTH);
-		//glPushMatrix();
-		//coordinate with Mark to see how he wants to put image
-		//tiles in the world
-		//also coordinate with Adam to work out collision detection
-		/*if (i%2 == 0) 
-		  glColor3ub(255,0,0);
-		  else 
-		  glColor3ub(0,255,0);*/
 		glBindTexture(GL_TEXTURE_2D, floorTexture);
-		//int j = 0;
 		float position;
-		//#ifdef RTH
 		for (int j = 0 ; j < arr[i]; j++) {
 			//Need this variable for K&R line length
 			position = pix_center + (j*PIXEL_WIDTH) ;
 			platform_boxes[box_num].y = position; 
-			//#endif //RTH
 			glPushMatrix();
 			glBegin(GL_QUADS);
 			glTexCoord2f(0.0, 1.0);
@@ -143,45 +113,44 @@ void render_floor()
 			glVertex2i(w*i+pixels,h*j);
 			glEnd();
 			glPopMatrix();
-			//#ifdef RTH
 			box_num++;
 		}
-		//#endif //RTH
 	}
-	}
+}
 
-	//checks collision between octopus and platforms
-	void el_platform_collision(Game *g)
-	{
-		//if (g){};
-		for (int i = 0 ; i < total_boxes; i++) {
-			if(g->ship.pos[0] <= platform_boxes[i].x +
-					platform_boxes[i].width && 
-					g->ship.pos[0] >= platform_boxes[i].x -
-					platform_boxes[i].width &&
-					g->ship.pos[1] < platform_boxes[i].y +
-					platform_boxes[1].height &&
-					g->ship.pos[1] > platform_boxes[i].y -	
-					platform_boxes[1].height) {
-			/*	g->ship.pos[0] = platform_boxes[i].x +
-					platform_boxes[i].width;
-				g->ship.pos[1] = platform_boxes[i].y +
-					platform_boxes[i].height*/;
-			}
-		}	
-	}
+void el_platform_collision(Game *g)
+{
+	//if (g){};
+	for (int i = 0 ; i < total_boxes; i++) {
+		//cout << "DEBUG: i = " << i << endl;
+		if(g->ship.pos[0] <= platform_boxes[i].x +
+				platform_boxes[i].width && 
+				g->ship.pos[0] >= platform_boxes[i].x -
+				platform_boxes[i].width &&
+				g->ship.pos[1] < platform_boxes[i].y +
+				platform_boxes[1].height &&
+				g->ship.pos[1] > platform_boxes[i].y -	
+				platform_boxes[1].height) {
+		//	cout << "DEBUG: boxes\n";
+			g->ship.pos[0] = platform_boxes[i].x +
+			  platform_boxes[i].width;
+			  g->ship.pos[1] = platform_boxes[i].y +
+			  platform_boxes[i].height;
+		}
+	}	
+}
 
-	//float el_gravity(Game *g){
-	float el_gravity()
-	{
-		return octogravity;
-	}
+float el_gravity()
+{
+	return octogravity;
+}
 
-	void el_gravity_f(Game *g)
-	{
-		//if (g){};
-		cout<<"DEBUG: nasteroids = "<< g->nasteroids << endl;
-		g->ship.vel[1] += octogravity;
-	}
-
-	//void read_by_char(string filename)
+void el_gravity_f(Game *g)
+{
+	//if (g){};
+#ifdef DEBUG_GRAVITY	
+	cout<<"DEBUG: nasteroids = "<< g->nasteroids << endl;
+#endif //DEBUG_GRAVITY
+	g->ship.vel[1] += octogravity;
+}
+ //void read_by_char(string filename)
