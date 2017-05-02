b32d729e61f17cb7ea22df209c4553848659f8e9
abutnda <abutanda@csub.edu>
2017-05-02 15:51:20 -0700

    buttons

--- anaB.cpp
+++ anaB.cpp
@@ -24,10 +24,10 @@ void print_Ana()
     string ana ="ana" ;
     cout << ana << endl;
 }
-/*struct Vec
-  {
-  float x,y,z;
-  };*/
+struct Vec
+{
+    float x,y,z;
+};
 /*struct Shape
   {
   Rect r;
@@ -82,31 +82,96 @@ nbox = 0;
 
 
 struct Global {
-    float xres;
-    float yres;
+  //  float xres;
+   // float yres;
     int done;
     int nbox;
     Global (){
-	xres = 800;
-	yres = 600;
+//	xres = 800;
+//	yres = 600;
 	done = 0;
 	nbox = 0;
     }
 } g;
 
 void ab_init(){
-    //Button button[4];
-    for (int i=0; i<4;i++){
-
-	button[i].r.width=100;
-	button[i].r.height=10;
-	button[i].r.centerx =(float)xres/2.0-3.0*i;
-	button[i].r.centery =yres/2.0-3.0*i;
+//#define BUTTONS
+#ifdef BUTTONS
+	//button 1
+	button[i].r.width=475;
+	button[i].r.height=200;
+	//button[i].r.centerx =(float)xres/2.0-3.0*i;
+	//button[i].r.centery =yres/2.0-3.0*i;
+	button[i].r.centerx =(float)xres/2.0;
+	button[i].r.centery =(float)yres/2.0-50*i;
+	button[i].r.left = 150;
+	button[i].r.right = xres-150;
+	button[i].r.top = yres-200;
+	button[i].r.bot = yres-200-105;
 	button[i].click=0;
 	button[i].over=0;
 	button[i].down=0;
+	button[i].dcolor[0]=0.0f+(float)i/0.5;
+	button[i].dcolor[1]=0.4f;
+	button[i].dcolor[2]=0.7f;
     }
-    //int nbuttons=0;
+    //button 2
+
+	button[i].r.width=475;
+	button[i].r.height=200;
+	//button[i].r.centerx =(float)xres/2.0-3.0*i;
+	//button[i].r.centery =yres/2.0-3.0*i;
+	button[i].r.centerx =(float)xres/2.0;
+	button[i].r.centery =(float)yres/2.0-50*i;
+	button[i].r.left = 150;
+	button[i].r.right = xres-150;
+	button[i].r.top = yres-200;
+	button[i].r.bot = yres-200-105;
+	button[i].click=0;
+	button[i].over=0;
+	button[i].down=0;
+	button[i].dcolor[0]=0.0f+(float)i/0.5;
+	button[i].dcolor[1]=0.4f;
+	button[i].dcolor[2]=0.7f;
+	//button 3
+	
+	button[i].r.width=475;
+	button[i].r.height=200;
+	//button[i].r.centerx =(float)xres/2.0-3.0*i;
+	//button[i].r.centery =yres/2.0-3.0*i;
+	button[i].r.centerx =(float)xres/2.0;
+	button[i].r.centery =(float)yres/2.0-50*i;
+	button[i].r.left = 150;
+	button[i].r.right = xres-150;
+	button[i].r.top = yres-200;
+	button[i].r.bot = yres-200-105;
+	button[i].click=0;
+	button[i].over=0;
+	button[i].down=0;
+	button[i].dcolor[0]=0.0f+(float)i/0.5;
+	button[i].dcolor[1]=0.4f;
+	button[i].dcolor[2]=0.7f;
+    //button 4
+
+	button[i].r.width=475;
+	button[i].r.height=200;
+	//button[i].r.centerx =(float)xres/2.0-3.0*i;
+	//button[i].r.centery =yres/2.0-3.0*i;
+	button[i].r.centerx =(float)xres/2.0;
+	button[i].r.centery =(float)yres/2.0-50*i;
+	button[i].r.left = 150;
+	button[i].r.right = xres-150;
+	button[i].r.top = yres-200;
+	button[i].r.bot = yres-200-105;
+	button[i].click=0;
+	button[i].over=0;
+	button[i].down=0;
+	button[i].dcolor[0]=0.0f+(float)i/0.5;
+	button[i].dcolor[1]=0.4f;
+	button[i].dcolor[2]=0.7f;
+#endif //buttons
+
+	//int nbuttons=0;
     /*strcpy(button[nbuttons].text, "How to Play");
       nbuttons++;
       strcpy(button[nbuttons].text, "Controls");
@@ -136,12 +201,13 @@ void ana_show_help(){
     int offsetx =150;
     int offsety = 200;
     for (int i=0;i<4;i++){
-	if (x >= button[i].r.center - button[i].r.width &&
-		x <= button[i].r.center + button[i].r.width &&
-		y >= button[i].r.center-button[i].r.height &&
-		y <= button[i].r.center+ button[i].r.height){
-	    glColor3f(1.0f, 0.0f, 0.0f);
 
+	/*if (x >= button[i].r.center - button[i].r.width &&
+	  x <= button[i].r.center + button[i].r.width &&
+	  y >= button[i].r.center-button[i].r.height &&
+	  y <= button[i].r.center+ button[i].r.height){
+	  */
+	if(button[i].over){
 	    glLineWidth(2);
 	    glBegin(GL_LINE_LOOP);
 	    glVertex2i(button[i].r.left-2,  button[i].r.bot-2);
@@ -151,113 +217,127 @@ void ana_show_help(){
 	    glVertex2i(button[i].r.left-2,  button[i].r.bot-2);
 	    glEnd();
 	    glLineWidth(1);
+	    glColor3fv(button[i].dcolor);
 	}
-	glColor3f(0.0,1.0/(2*i),2.0);
+	//	glColor3f(1.0f, 0.0f, 0.0f);
+	/*glLineWidth(2);
+	  glBegin(GL_LINE_LOOP);
+	  glVertex2i(button[i].r.left-2,  button[i].r.bot-2);
+	  glVertex2i(button[i].r.left-2,  button[i].r.top+2);
+	  glVertex2i(button[i].r.right+2, button[i].r.top+2);
+	  glVertex2i(button[i].r.right+2, button[i].r.bot-2);
+	  glVertex2i(button[i].r.left-2,  button[i].r.bot-2);
+	  glEnd();
+	  glLineWidth(1);*/
+	else{		
+	    glColor3f(0.0,1.0/(2*i),2.0);
+	}
 	glBegin(GL_QUADS);
 	glVertex2i((xres-offsetx),((offsety)*3)- i*offset);
 	glVertex2i((xres-offsetx),(yres-offsety)-i*offset);
 	glVertex2i((offsetx),(yres-offsety)-i*offset);
 	glVertex2i((offsetx),((offsety)*3)-i*offset);
 	glEnd();
-	cout<<"buttons drawn"<<endl;
-    }	 
+
+	//	cout<<"buttons drawn"<<endl;
+    }
     r.bot= button[0].r.centery+190;
     r.left =button[0].r.centerx;
     ggprint12(&r,0,0x00000000,"Controls");
-    r.bot= button[0].r.centery+90;
-    r.left = button[0].r.centerx;
+    r.bot= button[1].r.centery+90;
+    r.left = button[1].r.centerx;
     ggprint12(&r,0,0x00000000,"How to play");
-    r.bot= button[0].r.centery-15;
-    r.left = button[0].r.centerx;
+    r.bot= button[2].r.centery-15;
+    r.left = button[2].r.centerx;
     ggprint12(&r,0,0x00000000,"Release Notes");
-    r.bot= button[0].r.centery-120;
-    r.left =button[0].r.centerx;
+    r.bot= button[3].r.centery-120;
+    r.left =button[3].r.centerx;
     ggprint12(&r,0,0x00000000,"About");
 
-}
-
-void abcm(XEvent *e)
-{
-    cout<<"in abcm"<<endl;
-    static int savex = 0;
-    static int savey = 0;
-    int x,y;
-    int lbutton=0;
-    int rbutton=0;
-    if (e->type == ButtonRelease){
-       cout<<"abcm 1"<<endl;
-
-       return;
-       }
-    if (e->type == ButtonPress) {
-	if (e->xbutton.button==1) {
-	    int lbutton = 1;
-	    cout<<"abcm 2"<<endl;
-	}
-	if (e->xbutton.button==3) {
-	    cout<<"abcm 3"<<endl;
-	    rbutton=1;
-	    if (rbutton) {}
-	}
     }
-    cout <<"abcm 4"<<endl;
-    x = e->xbutton.x;
-    y = e->xbutton.y;
-    y = yres - y;
-    if (savex != e->xbutton.x || savey != e->xbutton.y) {
-	savex = e->xbutton.x;
-	savey = e->xbutton.y;
 
-    	cout <<"abcm 5"<<endl;
-    }
-		
-    cout <<"abcm 6"<<endl;
-    //int i= 0;
-    for (int i=0; i<4; i++) {
-	button[i].over = 0;
-	cout<<"for loop"<<endl;
-	if (x >= button[i].r.center - button[i].r.width &&
-		x <= button[i].r.center + button[i].r.width &&
-		y <= button[i].r.center+button[i].r.height &&
-		y >= button[i].r.center- button[i].r.height){
-	    button[i].over=1;
-	    cout<<"over button: "<<i<<endl;
+    void abcm(XEvent *e)
+    {
+	cout<<"in abcm"<<endl;
+	static int savex = 0;
+	static int savey = 0;
+	int x,y;
+	int lbutton=0;
+	int rbutton=0;
+	if (e->type == ButtonRelease){
+	    cout<<"abcm 1"<<endl;
 
-	    //esteban put just for compile. get rid of later
-	    int lbutton = 1;
-	    if (button[i].over) {
-		if (lbutton) {
-		    switch(i) {
-			case 0:
-			    cout << "Controls"<<endl;
-			    system("firefox http://www.cs.csub.edu/~abutanda/3350/help_menu/help_menu.php");
-			    break;
-			case 1:
-			    cout << "How to play"<<endl;
-			    system("firefox http://www.cs.csub.edu/~abutanda/3350/help_menu/help_menu.php");
-			    break;
-			case 2:
-			    cout << "Release Notes"<<endl;
-			    system("firefox http://www.cs.csub.edu/~abutanda/3350/help_menu/help_menu.php");
-			    break;
-			case 3:
-			    cout << "About"<<endl;
-			    system("firefox http://www.cs.csub.edu/~abutanda/3350/help_menu/help_menu.php");
-			    break;
+	    return;
+	}
+	if (e->type == ButtonPress) {
+	    if (e->xbutton.button==1) {
+		int lbutton = 1;
+		cout<<"abcm 2"<<endl;
+	    }
+	    if (e->xbutton.button==3) {
+		cout<<"abcm 3"<<endl;
+		rbutton=1;
+		if (rbutton) {}
+	    }
+	}
+	cout <<"abcm 4"<<endl;
+	x = e->xbutton.x;
+	y = e->xbutton.y;
+	//y = yres - y;
+	if (savex != e->xbutton.x || savey != e->xbutton.y) {
+	    savex = e->xbutton.x;
+	    savey = e->xbutton.y;
+
+	    cout <<"abcm 5"<<endl;
+	}
+
+	cout <<"abcm 6"<<endl;
+	//int i= 0;
+	for (int i=0; i<4; i++) {
+	    button[i].over = 0;
+	    cout<<"for loop"<<endl;
+	    if (x >= button[i].r.centerx - button[i].r.width &&
+		    x <= button[i].r.centerx + button[i].r.width &&
+		   y >= button[i].r.centery-button[i].r.height &&
+		   y <= button[i].r.centery+ button[i].r.height){
+		button[i].over=1;
+		cout<<"if loop"<<i<<endl;
+
+		//esteban put just for compile. get rid of later
+		int lbutton = 1;
+		if (button[i].over) {
+		    if (lbutton) {
+			switch(i) {
+			    case 0:
+				cout << "Controls"<<endl;
+				//system("firefox http://www.cs.csub.edu/~abutanda/3350/help_menu/help_menu.php");
+				break;
+			    case 1:
+				cout << "How to play"<<endl;
+				//system("firefox http://www.cs.csub.edu/~abutanda/3350/help_menu/help_menu.php");
+				break;
+			    case 2:
+				cout << "Release Notes"<<endl;
+				//system("firefox http://www.cs.csub.edu/~abutanda/3350/help_menu/help_menu.php");
+				break;
+			    case 3:
+				cout << "About"<<endl;
+				//system("firefox http://www.cs.csub.edu/~abutanda/3350/help_menu/help_menu.php");
+				break;
+			}
 		    }
 		}
 	    }
+	    }
+
+	    return;
 	}
-    }
-
-    return;
-}
 
 
 
 
-//boxes and add menu options are in the game need.
-//need to link each box to a link and make it click-able.  
-//link to a webpage
-//system("picture")
-//http://www.cs.csub.edu/~abutanda/3350/help_menu/help_menu.php
+	//boxes and add menu options are in the game need.
+	//need to link each box to a link and make it click-able.  
+	//link to a webpage
+	//system("picture")
+	//http://www.cs.csub.edu/~abutanda/3350/help_menu/help_menu.php
